35ba39685f32853460a7b55688ec026988eab313
stp4 <w.peter@statistik-peter.at>
2018-11-16 09:24:38 +0100

    Mediation2 umbenannt auf Mediation. Test erstellt sowie die eingaben von Formeln ist jetzt möglich.

--- R/regression-mediation.R
+++ R/regression-mediation.R
@@ -1,7 +1,11 @@
-#' @rdname Mediation
-#' @title Mediation Funktion
-#' @name Mediation
-#' @description Einfache Mediation und Moderationsanalyse mit Sobel-Test
+
+
+# "Fri Nov 16 07:45:40 2018"
+
+#'  Mediation
+#'
+#'
+#' Einfache Mediation und Moderationsanalyse mit Sobel-Test
 #' Mediation ist gegeben wenn alle Modelle signifikant (1) und (4)
 #'
 #'   (1) Y~X
@@ -11,10 +15,13 @@
 #'   Moderation ist gegeben wenn die Interaktion (X:M) signifikant ist
 #'
 #'   (3) Y~ X + M + X:M
-#' @param model_y_x Model y~x
-#' @param model_y_xm Model y~x+m
-#' @param model_m_x Model m~x
-#' @param model_inter_xm Model y~x*m
+#'   
+#'   Ein Beispiel mit Laavan findet sich unter
+#'   https://paolotoffanin.wordpress.com/2017/05/06/multiple-mediator-analysis-with-lavaan/
+#' @param y_x Model y~x
+#' @param y_xm Model y~x+m
+#' @param m_x Model m~x
+#' @param inter_xm Model y~x*m
 #' @param X Einflussvaria´ble
 #' @param M Moderator
 #' @param type fuer Regression APA_Tabelle()
@@ -23,194 +30,229 @@
 #' @return String (HTML)
 #' @export
 #' @examples
-#'
-#' library(effects)
-#' library(stp25)
-#' graphics.off()
-#' setwd("C:/Users/wpete/Dropbox/3_Forschung/10_temp")
-#' Projekt("html", "Mediation")
-#'
-#' Text(
-#'   "
-#'   Mediation ist gegeben wenn alle Modelle signifikant (1) und (4)
-#'
-#'   (1) Y~X
-#'   (2) Y~X+M  (wenn hier M signifikant ist => partielle Mediation)
-#'   (4) M~X
-#'
-#'   Moderation ist gegeben wenn die Interaktion (X:M) signifikant ist
-#'
-#'   (3) Y~ X + M + X:M
-#'   "
+#'#' 
+#' ## https://paolotoffanin.wordpress.com/2017/05/06/multiple-mediator-analysis-with-lavaan/
+#' ## simpleMediation
+#' #Projekt("html", "Test")
+#' set.seed(1234)
+#' Motivation <- rnorm(100)
+#' Lerndauer <- 0.5 * Motivation + rnorm(100)
+#' Note <- 0.7 * Lerndauer + rnorm(100)
+#' dat <- data.frame(Note = Note,
+#'                   Lerndauer = Lerndauer,
+#'                   Motivation = Motivation)
+#' 
+#' dat <- stp25aggregate::Label(dat,
+#'                              Note = "Note (Ziel-Variable)" ,
+#'                              Lerndauer = "Lerndauer (Mediator)",
+#'                              Motivation = "Motivation (Einfluss-Variable)")
+#' Tabelle2(dat, Note, Lerndauer, Motivation)
+#' Mediation(
+#'   Note ~ Motivation,
+#'   Note ~ Motivation + Lerndauer,
+#'   Lerndauer ~ Motivation,
+#'   Note ~ Motivation * Lerndauer,
+#'   dat,
+#'   digits = 2,
+#'   caption = "Einfache Mediation und Moderationsanalyse mit Sobel-Test"
 #' )
-#'
-#' # Using the Jessor data discussed in Preacher and Kelley (2011), to illustrate
-#' # the methods based on summary statistics.
-#'
-#' # An  example data (from Hayes)
-#' DF <- rbind(
-#'   c(-5.00, 25.00, -1.00),
-#'   c(-4.00, 16.00, 2.00),
-#'   c(-3.00, 9.00, 3.00),
-#'   c(-2.00, 4.00, 4.00),
-#'   c(-1.00, 1.00, 5.00),
-#'   c(.00, .00, 6.00),
-#'   c(1.00, 1.00, 7.00),
-#'   c(2.00, 4.00, 8.00),
-#'   c(3.00, 9.00, 9.00),
-#'   c(4.00, 16.00, 10.00),
-#'   c(5.00, 25.00, 13.00),
-#'   c(-5.00, 25.00, -1.00),
-#'   c(-4.00, 16.00, 2.00),
-#'   c(-3.00, 9.00, 3.00),
-#'   c(-2.00, 4.00, 4.00),
-#'   c(-1.00, 1.00, 5.00),
-#'   c(.00, .00, 6.00),
-#'   c(1.00, 1.00, 7.00),
-#'   c(2.00, 4.00, 8.00),
-#'   c(3.00, 9.00, 9.00),
-#'   c(4.00, 16.00, 10.00),
-#'   c(5.00, 25.00, 13.00))
-#' colnames(DF)<-c("emo", "soz.stress", "ausbildung")
-#' DF<- as.data.frame(DF)
-#'
-#' #-----------------------------------------------------
-#'
-#' Text("Y=emo
-#'      X=soz.stress
-#'      Mediator/Moderator = ausbildung ")
-#' APA2( ~ emo + soz.stress + ausbildung, DF)
-#' model1 <-  lm(emo ~ soz.stress , DF)
-#' model2 <-  lm(emo ~ soz.stress + ausbildung , DF)
-#' model3 <-  lm(ausbildung ~  soz.stress , DF)
-#' model4 <-  lm(emo ~ soz.stress * ausbildung , DF)
-#'
-#' Mediation2(model1, model2, model3, model4)
-#'
-#'
-#' DF<- dapply2(DF, function(x)  scale(x))
-#' model1 <-  lm(emo ~ soz.stress , DF)
-#' model2 <-  lm(emo ~ soz.stress + ausbildung , DF)
-#' model3 <-  lm(ausbildung ~  soz.stress , DF)
-#' model4 <-  lm(emo ~ soz.stress * ausbildung , DF)
-#'
-#' Mediation2(model1, model2, model3, model4)
-#' windows(4, 4)
-#' plot(
-#'   allEffects(model4),
-#'   multiline = TRUE,
-#'   main = "", x.var="soz.stress",
-#'   key.args = list(border = 0, columns = 2)
-#' )
-#' SaveData()
-#'
-#'
-#'
-#' End()
+#' 
+#' model <- ' # direct effect
+#' Note ~ c*Motivation
+#' # mediator
+#' Lerndauer ~ a*Motivation
+#' Note ~ b*Lerndauer
+#' # indirect effect (a*b)
+#' indir := a*b
+#' # total effect
+#' total := c + (a*b)
+#' '
+#' 
+#' #xyplot(Note ~ value |
+#' #         variable,
+#' #       melt2(dat, Motivation, Lerndauer, by =  ~ Note))
+#' 
+#' 
+#' fit <-  lavaan::sem(model, data = dat)
+#' APA2(fit)
+#' #End()
 
-Mediation2<- function( model_y_x,
-                       model_y_xm,
-                       model_m_x,
-                       model_y_inter_xm=NULL,
-                       X=model_info(model_m_x)$x ,
-                       M= model_info(model_m_x)$y,
-                       caption="",
-                       type="tex",
-                       ...){
 
-  if(!is.null(model_y_inter_xm))
-  APA_Table (model_y_x, model_y_xm, model_y_inter_xm, model_m_x,
-             caption=caption,
-             type="tex",
-             names=c("(1) Y~X", "(2) Y~X+M", "(3) Y~X*M" , "(4) M~X"))
+Mediation <- function(y_x,
+                      y_xm,
+                      m_x,
+                      y_inter_xm = NULL,
+                      data = NULL,
+                      caption = "",
+                      note = "",
+                      output = which_output(),
+                      
+                      
+                      include.parameter = TRUE,
+                      include.sobl = TRUE,
+                      z.transform = TRUE,
+                      X = model_info(m_x)$x ,
+                      M = model_info(m_x)$y,
+                      
+                      type = "long2",
+                      
+                      ...) {
+  if (inherits(y_x, "formula")) {
+    data <- data[unique(c(
+      all.vars(y_x),
+      all.vars(y_xm),
+      all.vars(m_x),
+      all.vars(y_inter_xm)
+    ))]
+    
+    if (z.transform) {
+      data <- dapply1(data, function(x)
+        scale(as.numeric(x)))
+    }
+    y_x <- lm(y_x, data)
+    y_xm <- lm(y_xm, data)
+    m_x <- lm(m_x, data)
+    if (!is.null(y_inter_xm))
+      y_inter_xm <- lm(y_inter_xm, data)
+    
+  }
+  
+  res <- list()
+  
+  if (!is.null(y_inter_xm))
+    res$parameter <-
+    APA_Table (
+      y_x,
+      y_xm,
+      y_inter_xm,
+      m_x,
+      caption = caption,
+      type = type,
+      names = c("(1) Y~X", "(2) Y~X+M", "(3) Y~X*M" , "(4) M~X"),
+      output = FALSE,
+      ...
+    )
   else
-    APA_Table (model_y_x, model_y_xm, model_m_x,
-               caption=caption,
-               type="tex",
-               names=c("(1) Y~X", "(2) Y~X+M", "(4) M~X"))
-
-
-    Sobel_Test( model_y_x, model_y_xm, model_m_x, X=X, M=M) %>%
-    fix_format() %>%
-    Output(caption="Sobel's Test for Mediation")
-    Text("X=",X," M=", M)
-
+    res$parameter <-
+    APA_Table (
+      y_x,
+      y_xm,
+      m_x,
+      caption = caption,
+      type = type,
+      names = c("(1) Y~X", "(2) Y~X+M", "(4) M~X"),
+      output = FALSE,
+      ...
+    )
+  
+  
+  res$sobel <-
+    Sobel_Test(y_x, y_xm, m_x, X = X, M = M)
+  
+  res$model <- paste("X=", X, " M=", M)
+  
+  if (include.parameter)
+    Output(res$parameter, caption = caption, output = output)
+  if (include.sobl)
+    Output(
+      res$sobel,
+      caption = "Sobel's Test for Mediation",
+      note = res$model,
+      output = output
+    )
+  
+  invisible(res)
 }
 
 
 #-- Interne Funktion
 Sobel_Test <- function(model1,
-                  model2,
-                  model3,
-                  X=NULL,
-                  M=NULL){
+                       model2,
+                       model3,
+                       X = NULL,
+                       M = NULL,
+                       digits = 2) {
   mod1.out <- summary(model1)$coef
   mod2.out <- summary(model2)$coef
   mod3.out <- summary(model3)$coef
-  pred<- which(rownames(mod3.out) == X)
-  med<- which(rownames(mod2.out) == M)
-
-  indir <- mod3.out[pred,1] * mod2.out[med,1]
-
+  pred <- which(rownames(mod3.out) == X)
+  med <- which(rownames(mod2.out) == M)
+  
+  indir <- mod3.out[pred, 1] * mod2.out[med, 1]
+  
   effvar <- (mod3.out[pred, 1]) ^ 2 * (mod2.out[med, 2]) ^ 2 +
     (mod2.out[med, 1]) ^ 2 * (mod3.out[pred, 2]) ^ 2
   serr <- sqrt(effvar)
   zvalue = indir / serr
-
+  
   data.frame(
-    Source="indir",
-    B = (indir),
-    SE = (serr),
-    Z = (zvalue),
-    p.value = (2 * pnorm(-abs(zvalue))),
-    row.names = "Sobel"
+    Source = "indir",
+    B = Format2(indir, digits = digits),
+    SE = Format2(serr, digits = digits),
+    Z = rndr_Test_Statistic(zvalue),
+    p.value = rndr_P(2 * pnorm(-abs(zvalue)), FALSE),
+    row.names = "Sobel",
+    stringsAsFactors = FALSE
   )
-
+  
 }
 
 # @rdname Mediation
 # @export
-APA2.mediate.mer<- function (x,
-                             caption = "Causal Mediation Analysis",
-                             note="ACME (average causal mediation effect), ADE (average direct effect)",
-                             ...)
+APA2.mediate.mer <- function (x,
+                              caption = "Causal Mediation Analysis",
+                              note = "ACME (average causal mediation effect), ADE (average direct effect)",
+                              ...)
 {
   x <- mediation:::summary.mediate.mer(x)
   clp <- 100 * x$conf.level
-  txt<- "Causal Mediation Analysis"
+  txt <- "Causal Mediation Analysis"
   if (x$boot) {
     if (x$boot.ci.type == "perc") {
-      txt<- paste(txt, "(Nonparametric Bootstrap Confidence Intervals with the Percentile Method)")
+      txt <-
+        paste(txt,
+              "(Nonparametric Bootstrap Confidence Intervals with the Percentile Method)")
     }
     else if (x$boot.ci.type == "bca") {
-      txt<- paste(txt, "(Nonparametric Bootstrap Confidence Intervals with the BCa Method)")
+      txt <-
+        paste(txt,
+              "(Nonparametric Bootstrap Confidence Intervals with the BCa Method)")
     }
   }
   else {
-    txt<- paste(txt, "(Quasi-Bayesian Confidence Intervals)")
+    txt <- paste(txt, "(Quasi-Bayesian Confidence Intervals)")
   }
   if (!is.null(x$covariates)) {
-    txt<- paste(txt, "
-                (Inference Conditional on the Covariate Values Specified in `covariates') ")
+    txt <- paste(txt,
+                 "
+                 (Inference Conditional on the Covariate Values Specified in `covariates') ")
   }
-
-
-  txt<- paste(txt, "Mediator Groups:", x$group.m, "\\n\\n")
-  txt<- paste(txt, "Outcome Groups:", x$group.y, "\\n\\n")
-  txt<- paste(txt, "Output Based on Overall Averages Across Groups", "\\n\\n")
+  
+  
+  txt <- paste(txt, "Mediator Groups:", x$group.m, "\\n\\n")
+  txt <- paste(txt, "Outcome Groups:", x$group.y, "\\n\\n")
+  txt <-
+    paste(txt, "Output Based on Overall Averages Across Groups", "\\n\\n")
   isLinear.y <- ((class(x$model.y)[1] %in% c("lm", "rq")) ||
-                   (inherits(x$model.y, "glm") && x$model.y$family$family ==
-                      "gaussian" && x$model.y$family$link == "identity") ||
-                   (inherits(x$model.y, "survreg") && x$model.y$dist ==
-                      "gaussian") || (inherits(x$model.y, "merMod") &&
-                                        x$model.y@call[[1]] == "lmer"))
+                   (
+                     inherits(x$model.y, "glm") && x$model.y$family$family ==
+                       "gaussian" &&
+                       x$model.y$family$link == "identity"
+                   ) ||
+                   (
+                     inherits(x$model.y, "survreg") && x$model.y$dist ==
+                       "gaussian"
+                   ) || (inherits(x$model.y, "merMod") &&
+                           x$model.y@call[[1]] == "lmer")
+  )
   printone <- !x$INT && isLinear.y
   if (printone) {
     smat <- c(x$d1, x$d1.ci, x$d1.p)
     smat <- rbind(smat, c(x$z0, x$z0.ci, x$z0.p))
     smat <- rbind(smat, c(x$tau.coef, x$tau.ci, x$tau.p))
     smat <- rbind(smat, c(x$n0, x$n0.ci, x$n0.p))
-    rownames(smat) <- c("ACME", "ADE", "Total Effect", "Prop. Mediated")
+    rownames(smat) <-
+      c("ACME", "ADE", "Total Effect", "Prop. Mediated")
   }
   else {
     smat <- c(x$d0, x$d0.ci, x$d0.p)
@@ -223,24 +265,37 @@ APA2.mediate.mer<- function (x,
     smat <- rbind(smat, c(x$d.avg, x$d.avg.ci, x$d.avg.p))
     smat <- rbind(smat, c(x$z.avg, x$z.avg.ci, x$z.avg.p))
     smat <- rbind(smat, c(x$n.avg, x$n.avg.ci, x$n.avg.p))
-    rownames(smat) <- c("ACME (control)", "ACME (treated)",
-                        "ADE (control)", "ADE (treated)", "Total Effect",
-                        "Prop. Mediated (control)", "Prop. Mediated (treated)",
-                        "ACME (average)", "ADE (average)", "Prop. Mediated (average)")
+    rownames(smat) <- c(
+      "ACME (control)",
+      "ACME (treated)",
+      "ADE (control)",
+      "ADE (treated)",
+      "Total Effect",
+      "Prop. Mediated (control)",
+      "Prop. Mediated (treated)",
+      "ACME (average)",
+      "ADE (average)",
+      "Prop. Mediated (average)"
+    )
   }
-  colnames(smat) <- c("Estimate", paste(clp, "% CI Lower",
-                                        sep = ""), paste(clp, "% CI Upper", sep = ""), "p.value")
+  colnames(smat) <- c(
+    "Estimate",
+    paste(clp, "% CI Lower",
+          sep = ""),
+    paste(clp, "% CI Upper", sep = ""),
+    "p.value"
+  )
   #printCoefmat(smat, digits = 3)
-
-  txt<- paste(txt, "Sample Size Used:", x$nobs, "\\n\\n")
-
-  txt<- paste(txt, "Simulations:", x$sims, "\\n\\n")
-
-
+  
+  txt <- paste(txt, "Sample Size Used:", x$nobs, "\\n\\n")
+  
+  txt <- paste(txt, "Simulations:", x$sims, "\\n\\n")
+  
+  
   smat <-   prepare_output(fix_to_data_frame(smat),
                            caption = caption ,
-                           note=note)
-  smat%>% fix_format %>% Output()
+                           note = note)
+  smat %>% fix_format %>% Output()
   invisible(smat)
   }
 
@@ -267,13 +322,13 @@ APA2.mediate.mer<- function (x,
 #         } else {
 #           cat("Quasi-Bayesian Confidence Intervals\n")
 #         }
-#         
+#
 #         if (!is.null(x$covariates)) {
 #           cat(
 #             "(Inference Conditional on the Covariate Values Specified in `covariates')\n\n"
 #           )
 #         }
-#         
+#
 #         isLinear.y <-
 #           (
 #             (class(x$model.y)[1] %in% c("lm", "rq")) ||
@@ -287,9 +342,9 @@ APA2.mediate.mer<- function (x,
 #                   x$model.y$dist == "gaussian"
 #               )
 #           )
-#         
+#
 #         printone <- !x$INT && isLinear.y
-#         
+#
 #         if (printone) {
 #           # Print only one set of values if lmY/quanY/linear gamY without interaction
 #           smat <- c(x$d1, x$d1.ci, x$d1.p)
@@ -348,15 +403,15 @@ APA2.mediate.mer<- function (x,
 #                     ""),
 #             "p-value"
 #           )
-#         
-#         
-#         
+#
+#
+#
 #         #cat("\n")
 #         cat("Sample Size Used:", x$nobs, "\n")
 #         # cat("\n")
 #         cat("Simulations: ", x$sims, "\n\n")
-#         
-#         
+#
+#
 #         #invisible(x)
 #         if (print) {
 #           if (options()$prompt[1] == "HTML> ")
@@ -371,11 +426,11 @@ APA2.mediate.mer<- function (x,
 #         } else {
 #           smat
 #         }
-#         
-#         
-#         
+#
+#
+#
 #       }
-#     
+#
 #     med_summary <- summary(fit, ...)
 #     print_summary_mediate2(med_summary , print = print, caption = caption)
 #   }
---
+++ tests/testthat/test-reg-mediation.R
@@ -0,0 +1,29 @@
+context("test-reg-mediation")
+
+test_that("mediation works", {
+  set.seed(1234)
+  Motivation <- rnorm(100)
+  Lerndauer <- 0.5 * Motivation + rnorm(100)
+  Note <- 0.7 * Lerndauer + rnorm(100)
+  dat <- data.frame(Note = Note,
+                    Lerndauer = Lerndauer,
+                    Motivation = Motivation)
+  
+  
+  
+  res <- Mediation(
+    Note ~ Motivation,
+    Note ~ Motivation + Lerndauer,
+    Lerndauer ~ Motivation,
+    Note ~ Motivation * Lerndauer,
+    dat,
+    digits = 2,
+    output = FALSE
+  )
+  expect_equal[names(res) , c("parameter", "sobel", "model")]
+  
+  
+  expect_equal(res$sobel$B,
+               "0.28")
+  
+})
