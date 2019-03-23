75aebe3b70341b45cdeb41584d198775839510c5
stp4 <w.peter@statistik-peter.at>
2019-03-23 06:55:30 +0100

    Rang ist jetzt fehlerfrei.

--- R/describe-rank.R
+++ R/describe-rank.R
@@ -17,8 +17,73 @@
 #' @export
 #' @examples
 #'
-#' #'
 #' 
+#' require(stpvers)
+#' library(PlackettLuce)
+#' 
+#' nlv <- 5
+#' n <- 2 * 3 * nlv * 1
+#' set.seed(n)
+#' 
+#' DF <-
+#'   data.frame(
+#'     Geschlecht = gl(2, n / 2, labels = c("Maennlich", "Weiblich")),
+#'     Alter = gl(4, n / 4,   labels = c("20-29", "30-39", "40-49", "50-59")),
+#'     Landwirtschaft = gl(2, n / 2, labels = c("konventionell", "biologisch"))
+#'   )
+#' 
+#' Attribute <-
+#'   as.data.frame(t(apply(matrix(NA, ncol = n, nrow = 5), 2,
+#'                         function(x)
+#'                           sample.int(5))))
+#' 
+#' Attribute[1, ] <- c(5, 1, 4, 2, 3)
+#' Attribute[2, ] <- c(5, 1, 4, 2, 3)
+#' Attribute[3, ] <- c(5, 2, 4, 3, 1)
+#' Attribute[4, ] <- c(5, 1, 4, 3, 2)
+#' Attribute[5, ] <- c(5, 1, 4, 3, 2)
+#' 
+#' Attribute[21, ] <- c(1, 2, 5, 4, 3)
+#' Attribute[22, ] <- c(1, 4, 5, 3, 2)
+#' Attribute[23, ] <- c(2, 5, 1, 4, 3)
+#' Attribute[24, ] <- c(1, 4, 2, 5, 3)
+#' Attribute[25, ] <- c(1, 4, 3, 5, 2)
+#' 
+#' attribute  <- c("Verfuegbarkeit",
+#'                 "Vielfalt",
+#'                 "Qualitaet",
+#'                 "Geschmack",
+#'                 "Preis")
+#' 
+#' Attribute<- dapply2(Attribute, function(x) factor(x, 1:5, attribute))
+#' 
+#' DF <- cbind(DF, Attribute)
+#' 
+#' head(DF)
+#' 
+#' res <-
+#'   Rangreihe( ~ V1+V2+V3+V4+V5,
+#'              DF, include.percent=FALSE, order=FALSE, include.na=FALSE,
+#'              caption="Produkte aus konventioneller und biologischer  Landwirtschaft")
+#' 
+#' res$input
+#' names(res)
+#' x<- res$res
+#' 
+#' 
+#' 
+#' 
+#' 
+#' R <- as.rankings(res$items, res$input)
+#' 
+#' mod <- PlackettLuce( R )
+#' coef(mod)
+#' 
+#' 
+#' summary(mod)
+#' x$pc <-  round(coef(mod, log = FALSE) ,2)
+#' x$log.pc <- round(coef(mod, log = TRUE) ,2)
+#' x[order(x$pc,decreasing=TRUE),] 
 #' 
 #' 
 #' 
@@ -131,7 +196,7 @@ Rangreihe.formula <- function(x,
                               subset,
                               na.action = na.pass,
                               ...) {
-  X <- Formula_Data(x, data, subset, na.action)
+  X <- stp25formula::Formula_Data(x, data, subset, na.action)
   if (is.null(X$xname)) {
     Rangreihe.default(
       items = X$Y_data,
@@ -158,10 +223,13 @@ Rangreihe.formula <- function(x,
 #' @rdname Rangreihe
 #'
 #' @param items data.frame
-#' @param include.percent,include.freq,include.mean,include.z Auswerten
 #' @param groups gruppen
-#' @param order,decreasing sortieren
-#' @param N Stichprobe
+
+#' @param input  Format der Items c("ranking", "ordering"),
+#' @param output an stp25output
+#' @param include.percent,include.freq,include.mean,include.z,include.na was soll ausgewertet werden
+#' @param order,decreasing sortierung
+
 #' @param digits.mean digits
 #' @export
 Rangreihe.default <- function (items,
@@ -172,43 +240,41 @@ Rangreihe.default <- function (items,
                                include.freq=TRUE,
                                include.mean=TRUE,
                                include.z=TRUE,
+                               include.na=TRUE,
                                groups=NULL,
-                               order = TRUE,
-                               decreasing = TRUE,
-                               N = if (is.null(groups))
-                                 nrow(items)
-                               else
-                                 nrow(na.omit(groups)),
-                               # info = FALSE,
-                               digits.mean = 2,
+                               order = TRUE, decreasing = TRUE, digits.mean = 2,
+                                
+                               input = NULL,  #c("ranking", "ordering"),
+                             
                                ...)
 {
-  if (!stp25stat::is_all_identical2(items))
-    warning(
-      "Das Skalenniveau in der Rangreihe ist unterschiedlich. Moeglicherweise stimmen die Ergebnisse nicht!"
-    )
+  # @param rankings 
+  # @param N Stichprobe
+ N <- if (is.null(groups)) nrow(items) 
+       else nrow(na.omit(groups))
+                               
+ rankings <-  NULL #  Rang <- 1. 2. 3. usw
+ inpt <-  guess_input(items) 
   
-  if (!is.null(groups)) {
+  if ( !is.null(groups) ) {
     data_by_group <-
-      split(items, groups, sep = "___") #-- seperator fuer mehr als ein Faktor
-    r <- lapply(data_by_group, Calc_Rank)
-    res<- NULL
-    
-    for( i in names(r)) {
+      split(inpt$items, groups, sep = "___") #-- seperator fuer mehr als ein Faktor
+    r <- lapply(data_by_group, Calc_Rank, rankings=inpt$rankings, 
+                include.na=include.na)
+    res <- NULL
+    for( i in names(r) ) {
       res_1 <- format_rank(
         r[[i]],
         include.mean, include.z,
         include.percent, include.freq,
         digits.mean, order, decreasing
       )
-      
-      res<- rbind(res, cbind(Group=i, res_1))  
-      r$mean =   rbind(   cbind(r[[i]]$mean, group=i),  r$mean   )
-      
+      res <- rbind(res, cbind(Group=i, res_1))  
+      r$mean <- rbind(cbind(r[[i]]$mean, group=i), r$mean)
     }
   }
   else{
-    r  <- Calc_Rank(items)
+    r  <- Calc_Rank(inpt$items, rankings=inpt$rankings,  include.na=include.na)
     res <- format_rank(
       r,
       include.mean,
@@ -229,45 +295,124 @@ Rangreihe.default <- function (items,
     output=output,
     ...)
   
+  r$rankings  <- rankings
+  r$input<- inpt$input
   r$res <- res
+  r$items <- inpt$items
+  r$groups <- groups
+  
   invisible(r)
   
 }
 
 
-
-
-format_rank <- function(r,
-                        include.mean,
-                        include.z,
-                        include.percent,
-                        include.freq,
-                        digits.mean,
-                        order,
-                        decreasing) {
+#' @param items Data.frame
+#' @param rankings welche Raenge
+#' @param input c("ranking", "ordering")
+#'
+#' @return   list(RankByRow=RankByRow, rankings=rankings, input=input)
+#' @noRd
+guess_input <- function(items,
+                        rankings = NULL,
+                        input = NULL) {
   
-  if (include.mean & include.z)
-    rmean <- r$mean[, c( "mean","sd", "z.score")]
-  else if (include.z)
-    rmean <- r$mean[, "z.score"]
-  else if (include.mean)
-    rmean <- r$mean[, c( "mean","sd")]
+  
+  if (!is.table(input)) {
+  if (is.null(input)) {
+    if (is.factor(items[, 1]) | is.character(items[, 1])) {
+      RankByRow <- FALSE
+      input <- "ordering"
+     
+    }
+    else {
+      RankByRow <- TRUE
+      input <- "ranking"
+    }
+  } else {
+    if (input == "ordering")
+      RankByRow <- FALSE
+    else
+      RankByRow <- TRUE
+  }
+  if (RankByRow) {
+    if (is.numeric(items[, 1])) {
+      rankings <- unique(unlist(lapply(items,
+                                       function(x)
+                                         levels(factor(
+                                           x
+                                         )))))
+    }
+    else if (!is_all_identical2(items)) {
+      rankings <- unique(unlist(lapply(items,
+                                       function(x)
+                                         levels(factor(
+                                           x
+                                         )))))
+      warning(
+        "Das Skalenniveau in der Rangreihe ist unterschiedlich. Moeglicherweise stimmen die Ergebnisse nicht!"
+      )
+    }
+    else if (is.factor(items[, 1])) {
+      rankings <- levels(items[, 1])
+    }
+    else{
+      return (NULL)
+    }
+  }
   else
-    rmean <- NULL
+    rankings <- 1:ncol(items)
   
-  res <- as.data.frame(
-    cbind(
-      Items = r$labels,
-      if (include.percent & include.freq)
-        stp25rndr::rndr_percent(r$rel.freq * 100, r$freq, return_as_vector = FALSE)
-      else if (include.percent)
-        stp25rndr::rndr_percent(r$rel.freq * 100, return_as_vector = FALSE)
-      else
-        r$freq,
-      stp25rndr::Format2(rmean, digits.mean)
-    ),
-    stringsAsFactors = FALSE
-  )
+  if (!RankByRow)
+    items <- transpose(items)
+  
+  }
+  
+  list(
+    items=items,
+    RankByRow = RankByRow,
+    rankings = rankings,
+    input = input)
+}
+
+
+
+
+#' @noRd
+format_rank <- function(r, include.mean, include.z,
+                        include.percent, include.freq,
+                        digits.mean,
+                        order, decreasing) {
+  res <- data.frame( Items = r$labels,
+                     stringsAsFactors = FALSE )
+  if (include.percent & include.freq)
+    res <- cbind( res,
+                  stp25rndr::rndr_percent(r$rel.freq * 100, 
+                                          r$freq, 
+                                          return_as_vector = FALSE),
+                  stringsAsFactors = FALSE)
+  else if (include.percent)
+    res <- cbind( res,
+                  stp25rndr::rndr_percent(r$rel.freq * 100, 
+                                          return_as_vector = FALSE),
+                  stringsAsFactors = FALSE)
+  else{
+    res <- cbind(res, as.data.frame.array(r$freq))
+  }
+ 
+    
+  if (include.mean & include.z)
+    res <- cbind( res,
+                  stp25rndr::Format2(r$mean[, c( "mean","sd", "z.score")],
+                                     digits.mean)
+    )
+  else if (include.z)
+    res <- cbind( res,
+                  stp25rndr::Format2(r$mean[, "z.score"],
+                                     digits.mean))
+  else if (include.mean)
+    res <- cbind( res, 
+                  stp25rndr::Format2(r$mean[, c( "mean","sd")],
+                                     digits.mean))
   
   if (order)
     res[order(r$mean[, "z.score"],
@@ -277,47 +422,20 @@ format_rank <- function(r,
     res
 }
 
-#' Range transortieren
-#'
-#' @param x data.frame
-#'
-#' @return data.frame
-#' @examples
-#'
-#'   DF2 <-   data.frame(
-#'   R1 = factor(c("A", "A", "A", "C", "C", "A"),   c("A", "B", "C", "D")),
-#'   R2 = factor(c("B", "B", "B", "A", "B", "D"),   c("A", "B", "C", "D")),
-#'   R3 = factor(c("C", "C", "C", "B", "A", "B"),   c("A", "B", "C", "D"))
-#'   )
-#'   transpose(DF2)
-#' @noRd
-transpose <- function(x) {
-  last <- nrow(x)
-  x <- cbind(x, id = 1:last)
-  last_column <- ncol(x)
-  
-  x <- stp25aggregate::Melt2(x,
-                             id.vars = last_column,
-                             key = "variable",
-                             value = "rang")
-  
-  x <- tidyr::spread(x, rang, variable)
-  stp25stat:::dapply1(x[-1])
-  
-}
+
 
 
 #' @param x dataframe
-#' @param my_ranks levels
+#' @param rankings levels
 #'
 #' @return matrix
 #' @noRd
 
-table_apply <- function(x, my_ranks) {
+table_apply <- function(x, rankings) {
   tab <- t(sapply(x, function(x, ...) {
     table(factor(x, ...))
   },
-  levels = my_ranks, simplify = TRUE))
+  levels = rankings, simplify = TRUE))
   colnames(tab) <- paste0("R", colnames(tab))
   tab
 }
@@ -326,7 +444,6 @@ table_apply <- function(x, my_ranks) {
 
 
 #' @param x Items
-#' @param RankByRow Methode 
 #' @param sicherheit,q_wert Das LOCJ gilt nur wenn keine 0 vorhanden sind daher hier mit 99.9% wahrscheinlichkeit
 #' @param n Anzahl
 #'
@@ -335,67 +452,38 @@ table_apply <- function(x, my_ranks) {
 
 
 Calc_Rank <-
-  function(x,
-           RankByRow = if (is.factor(x[, 1]) |
-                           is.character(x[, 1]))
-             FALSE
-           else
-             TRUE,
+  function(x, 
+           rankings, 
+           include.na=TRUE,
            sicherheit = .001,
            q_wert = qnorm(1 - sicherheit),
            n = nrow(x),
+           
            mylabels=NULL
   ) {
     
-    if (n < 1) {
-      return(NULL)
-    }
+    if (n < 1) { return(NULL) }
     
     if (!is.table(x)) {
-      if (!RankByRow)
-        x <- transpose(x)
-      
       mylabels <- stp25aggregate::GetLabelOrName(x)
-      
-      if (!stp25stat::is_all_identical2(x)) {
-        my_ranks <- unique(unlist(lapply(x,
-                                         function(x)
-                                           levels(factor(
-                                             x
-                                           )))))
-      }
-      else if (is.factor(x[, 1]))
-        my_ranks <- levels(x[, 1])
-      else
-        my_ranks <- levels(factor(x[, 1]))
-      
-      tbl <- table_apply(x, my_ranks)
-      
-      # Test ob fehlende Werte
-      if (ncol(tbl) < nrow(tbl)) {
-        my_ranks <- c(my_ranks, ".NA")
-        tbl <- table_apply(x, my_ranks)
-        tbl[, ncol(tbl)] <- nrow(x) - rowSums(tbl)
-      }
+      if ( include.na ) rankings <- c(rankings, ".NA")
+      tbl <- table_apply(x, rankings)
+      tbl[, ncol(tbl)] <- nrow(x) - rowSums(tbl)
       rel_feq <- prop.table(tbl, 1)
-      
       x_mean <- sapply(x,  function(x) {
-        x[is.na(x)] <- length(my_ranks)
+        x[is.na(x)] <- length(rankings)
         c(m = mean(x, na.rm = TRUE),
           sd = sd(x, na.rm = TRUE),
           min = min(x, na.rm = TRUE),
           max = max(x, na.rm = TRUE),
           median = median(x, na.rm = TRUE)
-          
-        )
-      } )
-      
+        )})
     }
     else
     {
       mylabels<- rownames(x)
       names(mylabels)<- mylabels
-      my_ranks <- seq_len(ncol(x))
+      rankings <- seq_len(ncol(x))
       tbl <- x
       rel_feq <- prop.table(tbl, 1)
       mm <- rel_feq * matrix(rep(1:ncol(rel_feq),
@@ -418,7 +506,7 @@ Calc_Rank <-
     zeilen.sum <- rowSums(z.wert, na.rm = TRUE)
     zeilen.mittel <- zeilen.sum / (ncol(z.wert))
     z_score <- (mean(zeilen.mittel, na.rm = T) - zeilen.mittel) * -1
- 
+    
     my_mean <- data.frame(Items=mylabels,
                           mean = x_mean[1L, ],
                           sd = x_mean[2L, ],
@@ -427,8 +515,8 @@ Calc_Rank <-
                           max = x_mean[4L, ],
                           median = x_mean[5L, ],
                           z.score = z_score,
-                          # z.rang = length(my_ranks) - ((z_score + q_wert) / 
-                          #(q_wert * 2) * (length(my_ranks) - 1)),
+                          # z.rang = length(rankings) - ((z_score + q_wert) / 
+                          #(q_wert * 2) * (length(rankings) - 1)),
                           stringsAsFactors = FALSE
                           
     )
@@ -436,7 +524,8 @@ Calc_Rank <-
       freq = tbl,
       rel.freq = rel_feq,
       z.value = cbind(z.wert,
-                      z.sum = zeilen.sum, z.mittel = zeilen.mittel),
+                      z.sum = zeilen.sum, 
+                      z.mittel = zeilen.mittel),
       mean = my_mean,
       labels = mylabels
     )
@@ -464,4 +553,3 @@ Calc_Rank <-
 #          output=output,
 #          ...)
 # }
-
