ec625d5097426c31344181dc6a0c1a24e171c29e
stp4 <w.peter@statistik-peter.at>
2020-04-03 09:19:48 +0200

    random=~id

--- R/T-test.R
+++ R/T-test.R
@@ -42,6 +42,7 @@ APA.ScalarIndependenceTest <- function(x, ...) {
 #'  Berechnung von Mittelwerten und Mittelwertdifferenzen und des T-Test.
 #'
 #' @param var.equal,paired,alternative an t.Test var.equal = FALSE  "two.sided"
+#' @param random  formula bei paired muss zwingend eine eindeutige id oder Fallnummer vorhanden sein
 #' @param include.mean Mittelwerte mit SD
 #' @param include.d Cohens d
 #' @param include.mean.diff  Mittlere Differenzen
@@ -62,28 +63,38 @@ APA.ScalarIndependenceTest <- function(x, ...) {
 #'
 #' APA_Ttest(m~time, varanax, paired = TRUE)
 #'
+#'
+
+#'
 #' APA_Ttest(m~time, varanax, var.equal=TRUE)
 #' APA_Ttest(m~time, varanax, var.equal=FALSE)
 #' #broom::tidy(t.test(m~time, varanax, var.equal=TRUE))
 #' #broom::tidy(t.test(m~time, varanax, var.equal=FALSE))
 #'
-#'  
-APA_Ttest <- function(x,
-                      data,
-                      caption="T Test", note=alternative,
-                      output = stp25output::which_output(),
-                      var.equal = FALSE,
-                      paired = FALSE,
-                      alternative =  "two.sided",
-                      include.mean = TRUE,
-                      include.d = TRUE,
-                      include.mean.diff = TRUE,
-                      include.se = FALSE,
-                      type="t.test",
-                      digits = 2,
-                      ...) {
+#'  varanax$m[1] <- NA
+#' APA_Ttest(m ~ time, varanax, paired = TRUE, random=~nr, include.n=TRUE) 
+#' 
+#' 
+APA_Ttest  <- function(x,
+                       data,
+                       caption = "T Test",
+                       note = alternative,
+                       output = stp25output::which_output(),
+                       var.equal = FALSE,
+                       paired = FALSE,
+                       alternative =  "two.sided",
+                       include.mean = TRUE,
+                       include.d = TRUE,
+                       include.mean.diff = TRUE,
+                       include.se = FALSE,
+                       include.n = FALSE,
+                       type = "t.test",
+                       digits = 2,
+                       random = NULL,
+                       ...) {
   lhs <- all.vars(x[-3])
   rhs <- all.vars(x[-2])
+  id <- all.vars(random)
   result <- list()
   method <- alternative # Fehler abfangen
   for (r in rhs) {
@@ -93,74 +104,106 @@ APA_Ttest <- function(x,
     {
       for (l in lhs) {
         fm <- formula(paste(l, "~", r))
+        
+        if (paired) {
+          if (is.null(random))
+            stop(
+              "Bei paired =TRUE muss zwingend eine eindeutige id mit random=~id uebergeben werden!"
+            )
+          
+          dat <- data[c(id, l, r)]
+          if (!all(complete.cases(dat))) {
+            dat <-
+              na.omit(Wide(dat, key = r, value = l))
+            dat <-  Long(formula(paste(
+              "~", paste(names(dat)[-1], collapse = "+")
+            )),
+            dat,
+            key = r,
+            value = l)
+          }
+          # print(dat)
+          #  return(NULL)
+        }
+        else {
+          dat <- data[c(l, r)]
+        }
+        
         res <- t.test(
           fm,
-          data,
+          dat,
           var.equal = var.equal,
           paired = paired,
           alternative = alternative
         )
-
+        
         # print(aggregate(fm, data, function(x) x))
         res_t <- broom::tidy(res)
         method <- as.character(res_t$method)
         res_sig  <- APA(res)
         res_mean <-
-          Tabelle(fm, data, APA = TRUE, include.n = FALSE)[[1]]
-
+          Tabelle(fm, dat, APA = TRUE, include.n = include.n)[[1]]
+        
         ans <- res_mean[1]
-        if (include.mean)
-          ans <- cbind(ans, res_mean[2:3])
+        
+        if (include.mean) {
+          if (!include.n)
+            vr <- 2:3
+          else if (paired)
+            vr <- c(2:3, 5)
+          else
+            vr <- 2:5
+          ans <- cbind(ans, res_mean[vr])
+        }
         if (include.mean.diff) {
-          res_diff <- paste(Format2(res_t$estimate, digits = digits),
-                            rndr_CI(as.matrix(res_t[c("conf.low", "conf.high")]), digits = digits))
+          res_diff <- paste(
+            Format2(res_t$estimate, digits = digits),
+            rndr_CI(as.matrix(res_t[c("conf.low", "conf.high")]), digits = digits)
+          )
           
-       
+          
           ans <- cbind(ans, Differenz = res_diff)
         }
         if (include.d) {
-          ans <- cbind(ans, cohens.d = Format2(cohens.d(fm, data), 2))
+          ans <- cbind(ans, cohens.d = Format2(cohens.d(fm, dat), 2))
         }
         if (include.se) {
-          ans <- cbind(ans, SE = Format2(calculate_se(fm, data), 2))
+          ans <- cbind(ans, SE = Format2(calculate_se(fm, dat), 2))
         }
-
-
-        if(type == "t.test")  ans <- cbind(ans, T.test = res_sig)
+        
+        
+        if (type == "t.test")
+          ans <- cbind(ans, T.test = res_sig)
         else {
           res <- wilcox.test(fm,
-                             data,
+                             dat,
                              paired = paired,
                              alternative = alternative)
           ans <- cbind(ans, wilcox.test = APA(res))
           method <- as.character(res$method)
-          }
+        }
         if (is.null(ANS))
           ANS <- ans
         else
           ANS <- rbind(ANS, ans)
-
       }
+      
+      
     }
     else{
-      Text(
-        r,
-        "hat ",
-        nlevels(data[[r]]),
-        " levels und kann daher nicht mittels T-Test berechnet werden!"
-      )
+      Text(r, "hat ", nlevels(data[[r]])," levels und kann daher nicht mittels T-Test berechnet werden!")
     }
-
-    ANS <- prepare_output(ANS,
-                         caption=caption,note=note)
-    result[[l]] <- ANS
+    
+    result[[l]] <- prepare_output(ANS,
+                                  caption = caption,
+                                  note = note)
   }
-
-  Output(result,  output=output )
+  
+  Output(result,  
+         output = output)
+  
   invisible(result)
 }
-
-
 # Standard error
 ### Calculate standard error manually
 calculate_se <- function(x, data) {
